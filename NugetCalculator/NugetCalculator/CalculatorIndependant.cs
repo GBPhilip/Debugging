d27004159bf3e2050301e6e11dac97252a6b3e5a
Philip Sutton <10743454+GBPhilip@users.noreply.github.com>
2019-07-07 12:57:52 +0100

    Nuget package sourcelink

---
+++ NugetCalculator/NugetCalculator/CalculatorIndependant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Debugging.InterestInterface;
+
+namespace NugetCalculator
+{
+    public class CalculatorIndependant :IInterestCalculator
+    {
+        public int CalculatorSavingsInterest(int balance)
+        {
+            return (int)(balance * 5.5 / 1);
+        }
+    }
+}
